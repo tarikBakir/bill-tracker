ebfe2e2a7f36f3b4957dee8106eae7eed80a7b6c
tarikBakir <tarik.bakir@ruppin365.net>
2022-09-17 20:40:47 -0400

    Update registration.py

--- registration.py
+++ registration.py
@@ -1,19 +1,23 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
-
+from wtforms.validators import InputRequired, Length, Email, Regexp, EqualTo
+import email_validator
 from models.account import Account
-from models.user import User
-from database import db
 
 
 class RegistrationForm(Form):
     email = StringField('Email', validators=[InputRequired(), Length(1, 100), Email()])
-    username = StringField('Username',
-                           validators=[InputRequired(), Length(max=64, message='Your username is too many characters.'),
-                                       Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-                                              'Username must have only letters, ' 'numbers, dots or underscores')])
+    firstName = StringField('FirstName',
+                            validators=[InputRequired(),
+                                        Length(max=64, message='Your first name is too many characters.'),
+                                        Regexp('^[A-Za-z][A-Za-z]*$', 0,
+                                               'FirstName must have only letters')])
+    lastName = StringField('LastName',
+                           validators=[InputRequired(),
+                                       Length(max=64, message='Your first name is too many characters.'),
+                                       Regexp('^[A-Za-z][A-Za-z]*$', 0,
+                                              'FirstName must have only letters')])
     password = PasswordField('password',
                              validators=[InputRequired(), EqualTo('password2', message='passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[InputRequired()])
