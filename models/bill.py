87d256fe77157a8edaa431d6f51e97e3d0e6976d
tarikBakir <94920690+tarikBakir@users.noreply.github.com>
2022-09-30 01:03:01 +0300

    updated all models

--- models/bill.py
+++ models/bill.py
@@ -1,3 +1,5 @@
+import datetime
+
 from database import db
 
 
@@ -6,9 +8,10 @@ class Bill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.Text)
     name = db.Column(db.String(100))
-    paid = db.Column(db.String(3))
-    createDate = db.Column(db.Date)
-    modifiedDate = db.Column(db.Date)
-    dueDate = db.Column(db.Date)
-    companyCode = db.Column(db.Integer, db.ForeignKey('companies.code'), nullable=False)
-    categoryID = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    paid = db.Column(db.Boolean)
+    amount = db.Column(db.Float)
+    createDate = db.Column(db.Date, default=datetime.datetime.utcnow)
+    modifiedDate = db.Column(db.Date, default=datetime.datetime.utcnow)
+    dueDate = db.Column(db.Date, default=datetime.datetime.utcnow)
+    companyId = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=True)
+    categoryId = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
--- models/company.py
+++ models/company.py
@@ -3,8 +3,8 @@ from database import db
 
 class Company(db.Model):
     __tablename__ = 'companies'
-    code = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    phonenumber = db.Column(db.Integer)
-    email = db.Column(db.String(100), unique=True)
+    phoneNumber = db.Column(db.String(10))
+    email = db.Column(db.String(100))
     bill = db.relationship('Bill', backref='companies')
